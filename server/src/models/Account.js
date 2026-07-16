bbbed25a2b45d51893ffdd3b9604edf95d5f0858
Murphik123 <roman.ashabad@gmail.com>
2026-07-16 20:04:11 +0500

    Create Account model with schema and pre-save hook

---
+++ server/src/models/Account.js
@@ -0,0 +1,34 @@
+const mongoose = require('mongoose');
+
+const accountSchema = new mongoose.Schema({
+  userId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true,
+    unique: true
+  },
+  balance: {
+    type: Number,
+    default: 0,
+    min: 0
+  },
+  currency: {
+    type: String,
+    default: 'TMT'
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now
+  },
+  updatedAt: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+accountSchema.pre('save', function(next) {
+  this.updatedAt = Date.now();
+  next();
+});
+
+module.exports = mongoose.model('Account', accountSchema);
