794c2feb6707b8dd4482195dfc73add85004821a
Murphik123 <roman.ashabad@gmail.com>
2026-07-30 02:16:58 +0500

    Add logout route to authRoutes

--- server/src/routes/authRoutes.js
+++ server/src/routes/authRoutes.js
@@ -2,9 +2,8 @@ const express = require('express');
 const router = express.Router();
 const authController = require('../controllers/authController');
 
-// Пути должны быть относительно /api/auth
-// В итоге получится: POST /api/auth/login и POST /api/auth/register
-router.post('/login', authController.login);
 router.post('/register', authController.register);
+router.post('/login', authController.login);
+router.post('/logout', authController.logout);
 
 module.exports = router;
