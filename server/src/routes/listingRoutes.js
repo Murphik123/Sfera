87d86126fd9446d80cbed0c8383e98b728fec508
Murphik123 <roman.ashabad@gmail.com>
2026-08-05 07:52:00 +0500

    Change upload configuration path in listingRoutes

--- server/src/routes/listingRoutes.js
+++ server/src/routes/listingRoutes.js
@@ -8,13 +8,11 @@ const {
   deleteListing
 } = require('../controllers/listingController');
 const { protect } = require('../middleware/authMiddleware');
-const { upload } = require('../config/cloudinary');
+const { upload } = require('../config/upload');
 
-// Публичные маршруты
 router.get('/', getListings);
 router.get('/:id', getListingById);
 
-// Защищенные маршруты (поддержка до 5 изображений на запрос)
 router.post('/', protect, upload.array('images', 5), createListing);
 router.put('/:id', protect, upload.array('images', 5), updateListing);
 router.delete('/:id', protect, deleteListing);
