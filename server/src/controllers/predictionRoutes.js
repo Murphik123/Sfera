3da4ac647d013e75ec58fdf790ba6c7763a6df02
Murphik123 <roman.ashabad@gmail.com>
2026-08-09 02:15:48 +0500

    Delete server/src/controllers/predictionRoutes.js

--- server/src/controllers/predictionRoutes.js
+++
@@ -1,9 +0,0 @@
-// src/routes/predictionRoutes.js
-const express = require('express');
-const router = express.Router();
-const predictionController = require('../controllers/predictionController');
-
-// GET /api/predictions
-router.get('/', predictionController.getPredictions);
-
-module.exports = router;
